cf0cea3a63c01a82e82ae39a2d773694ae552c6b
ScottishSmokedSalmon <32573705+ScottishSmokedSalmon@users.noreply.github.com>
2023-04-18 23:59:08 +0400

    Navbar development
    Added navbar items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutLineRestaurantMenu } from 'react-icons/md';
 
+import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="app logo" />  
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensas"><a href="home">/Home</a></li>
+      <li className="p__opensas"><a href="about">/About</a></li>
+      <li className="p__opensas"><a href="menu">/Menu</a></li>
+      <li className="p__opensas"><a href="awards">/Awards</a></li>
+      <li className="p__opensas"><a href="contact">/Contact</a></li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;

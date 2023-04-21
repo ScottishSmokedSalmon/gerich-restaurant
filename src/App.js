3724696ce8d2efc601abc21d822ddb01be8ed154
ScottishSmokedSalmon <32573705+ScottishSmokedSalmon@users.noreply.github.com>
2023-04-21 23:18:01 +0400

    add:new functionality
    About us section complete

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ const App = () => (
   <div>
     <Navbar />
     <Header />
-    {/*<AboutUs />
+    <AboutUs />
     <SpecialMenu />
-    <Chef />
+    {/*<Chef />
     <Intro />
     <Laurels />
     <Gallery />

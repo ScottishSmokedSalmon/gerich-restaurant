cf9d80dd25e0038500bd7b15d8ae3b16a840cb1e
ScottishSmokedSalmon <32573705+ScottishSmokedSalmon@users.noreply.github.com>
2023-04-25 23:19:19 +0400

    add:new functionality
    Gallery section completed

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -5,31 +5,28 @@ import { images, data } from '../../constants';
 import './Laurels.css';
 
 const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
-   <div>
-    <img src={imgUrl} alt="award" />
-    <div className="app__laurelss_awards-card_content">
-      <p className="p__cormorant" style={{color: '#DCCA87'}}>{title}</p> 
-      <p className="p__cormorant">{subtitle}</p> 
-
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      <p className="p__opensans">{subtitle}</p>
     </div>
-   </div>
-)
+  </div>
+);
 
 const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
     <div className="app__wrapper_info">
       <SubHeading title="Awards & recognition" />
-      <h1 className="headtext_cormorant">Our Laurels</h1>
+      <h1 className="headtext__cormorant">Our Laurels</h1>
 
       <div className="app__laurels_awards">
         {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
       </div>
-
     </div>
 
-    <div className="app__wwrapper_img">
-      <img src={images.laurels} alt="laurels" />
-
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels_img" />
     </div>
   </div>
 );
